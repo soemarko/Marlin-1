fe0de582a35d857ab3090b1920f59bfe045b9271
soemarko <soemarko@users.noreply.github.com>
2021-05-16 21:46:00 +0700

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -70,7 +70,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(Soemarko Ridwan, v2.0.7 config)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Soemarko Ridwan, v2.0.8 config)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
